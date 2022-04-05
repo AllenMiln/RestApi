d2a93afc7eab60829ff76fe580af6bbbaa41e919
Бутыльченко Сергей Александрович <Butylchenko.S.Al@sberbank.ru>
2022-04-06 01:55:45 +0300

    TEST_COMMIT-000 - v3

--- src/main/java/com/interview/testprojectforinterview/dao/ExchangeRatesDaoImpl.java
+++ src/main/java/com/interview/testprojectforinterview/dao/ExchangeRatesDaoImpl.java
@@ -1,17 +1,15 @@
 package com.interview.testprojectforinterview.dao;
 
 import com.interview.testprojectforinterview.model.ExchangeRate;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.SneakyThrows;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -29,53 +27,53 @@ public class ExchangeRatesDaoImpl implements ExchangeRatesDao{
 
     public List<ExchangeRate> getAllExchangeRatesPerDay(String date) {
         List<ExchangeRate> exchangeRates = new ArrayList<>();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
-        try {
-            java.util.Date finalParsedDate = format.parse(date);
-            exchangeRates = jdbcTemplate.query("SELECT * FROM EXCHANGE_RATE WHERE DATE = ?",
-                    ps -> ps.setDate(1, new Date(finalParsedDate.getTime())),
-                    (rs, rowNum) -> ExchangeRate.builder()
-                            .numCode(rs.getString("NUM_CODE"))
-                            .charCode(rs.getString("CHAR_CODE"))
-                            .nominal(rs.getString("NOMINAL"))
-                            .name(rs.getString("NAME"))
-                            .value(rs.getString("VALUE"))
-                            .build()
-            );
-        } catch (ParseException e) {
-            e.getMessage();
-        }
+        exchangeRates = jdbcTemplate.query("SELECT * FROM EXCHANGE_RATE WHERE DATE = ?",
+                new PreparedStatementSetter() {
+                    @SneakyThrows
+                    @Override
+                    public void setValues(PreparedStatement ps) {
+                        ps.setDate(1, ExchangeRatesDaoImpl.this.parseDate(date));
+                    }
+                },
+                (rs, rowNum) -> ExchangeRate.builder()
+                        .numCode(rs.getString("NUM_CODE"))
+                        .charCode(rs.getString("CHAR_CODE"))
+                        .nominal(rs.getString("NOMINAL"))
+                        .name(rs.getString("NAME"))
+                        .value(rs.getString("VALUE"))
+                        .build()
+        );
 
         return exchangeRates;
     }
 
     @Override
     public void saveExchangeRates(List<ExchangeRate> exchangeRates, String date) {
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO EXCHANGE_RATE (NUM_CODE, CHAR_CODE, NOMINAL, NAME, VALUE, DATE) VALUES (?, ?, ?, ? ,? ,?)",
+                new BatchPreparedStatementSetter() {
+                    @SneakyThrows
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) {
+                        ps.setString(1, exchangeRates.get(i).getNumCode());
+                        ps.setString(2, exchangeRates.get(i).getCharCode());
+                        ps.setString(3, exchangeRates.get(i).getNominal());
+                        ps.setString(4, exchangeRates.get(i).getName());
+                        ps.setString(5, exchangeRates.get(i).getValue());
+                        ps.setDate(6, parseDate(date));
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return exchangeRates.size();
+                    }
+                });
+    }
+
+    private Date parseDate(String date) throws ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-
-        try {
-            java.util.Date finalParsedDate = format.parse(date);
-            jdbcTemplate.batchUpdate(
-                    "INSERT INTO EXCHANGE_RATE (NUM_CODE, CHAR_CODE, NOMINAL, NAME, VALUE, DATE) VALUES (?, ?, ?, ? ,? ,?)",
-                    new BatchPreparedStatementSetter() {
-                        @Override
-                        public void setValues(PreparedStatement ps, int i) throws SQLException {
-                            ps.setString(1, exchangeRates.get(i).getNumCode());
-                            ps.setString(2, exchangeRates.get(i).getCharCode());
-                            ps.setString(3, exchangeRates.get(i).getNominal());
-                            ps.setString(4, exchangeRates.get(i).getName());
-                            ps.setString(5, exchangeRates.get(i).getValue());
-                            ps.setDate(6, new Date(finalParsedDate.getTime()));
-                        }
-
-                        @Override
-                        public int getBatchSize() {
-                            return exchangeRates.size();
-                        }
-                    });
-        } catch (ParseException e) {
-            e.getMessage();
-        }
+        java.util.Date finalParsedDate = format.parse(date);
+        return new Date(finalParsedDate.getTime());
     }
 }
